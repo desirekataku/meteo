e2fdbe5c0214d8dcaaca0682a1e16e3d72527eb0
desirekataku <138031131+desirekataku@users.noreply.github.com>
2025-01-21 16:03:12 +0100

    lg pour fr

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ searchButton.addEventListener('click', () => {
 });
 
 function fetchWeather(city) {
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=fr`;
     fetch(url)
         .then(response => {
             if (!response.ok) {
